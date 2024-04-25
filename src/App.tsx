fd4e3b0b1faa7197a6b79c21c4a9aeaf040e2e96
Kyungjin Jung <kyoooungjin@gmail.com>
2024-04-25 19:25:36 +0900

    refactor: 필요없는 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import { Button } from '@februaar/design-system';
 
 const App = () => {
-  return (
-    <>
-      <Button
-        size="lg"
-        variant="primary"
-        color="serenity"
-        _hover={{ color: '#fff', backgroundColor: '#f7cac9' }}
-      >
-        버튼을 눌러보세요
-      </Button>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
